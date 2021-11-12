4a70fbccf59c9b5c50d54236f0788fa7c336d04e
Andrew Klokov <555135@mail.ru>
2021-11-12 14:17:14 +0300

    Add calc Net

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], int)
-print(f"Массив a:\n{a}\n")
-b = np.delete(a, -1, axis=1)
-print(f"Массив b:\n{b}\n")
+print(5+5+5+5)
+
+print(5
+     +6
+      +
+      5
+      +5)
+
